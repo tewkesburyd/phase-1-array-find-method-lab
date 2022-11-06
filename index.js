1e66ec87ab113ed901faa157b2e856780cabfc07
David Tewkesbury <tewkesburyd@hotmail.com>
2022-11-06 11:01:09 -0700

    commit tree 6ca62cea60bc705178b437e71dc43f6e30888f62

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const superbowlWin = (games) => {
+    const wonGame = games.find((game) => {
+        return game.result === 'W';
+    });
+    if (wonGame){
+        return wonGame.year
+}
+}
+
+
